ba2f016184755106617c24740efc111ef1d9d6b0
lizkavalski <lizkavalski@gmail.com>
2021-01-23 09:02:46 -0800

    working on the logic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './compontents/header';
 import Footer from './compontents/footer';
+import Challenge  from './compontents/challenge';
 
 function App() {
   return (
     <>
       <Header/>
+      <Challenge/>
       <Footer/>
     </>
   );
---
+++ src/compontents/challenge.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import {Box, Button, Grid, CircularProgress, Card} from'@material-ui/core/';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  noteCard:{
+    textAlign: 'center',
+    backgroundImage:'url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2F5729537%2Fstock-photo-whiteboard-isolated-on-white-background.html&psig=AOvVaw1CrCBcMLnvyFLKtXUgqOAZ&ust=1611507021059000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOigkMzBsu4CFQAAAAAdAAAAABAa)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    boxShadow: '.5em .5em .3em #9c9c9c',
+    paddingTop:'5em',
+    paddingBottom:'2em',
+    marginLeft:'5em',
+    marginRight:'5em',
+  },
+  noteCardWriting:{
+    textAlign: 'center', 
+    fontSize:'3em',
+    fontFamily:['Nanum Pen Script', 'cursive'],
+  },
+  buttonBox:{
+    textAlign: 'center',
+    paddingTop:'2em', 
+    paddingBottom:'4em',
+  },
+  nButton:{
+    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    textAlign: 'center',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px #9c9c9c',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+
+function Challenge(){
+  const [challenge, setChallenge] = useState('');
+  const [loading, setLoading]= useState(true);
+  const [fetching, setFetching]=useState(false);
+  const styles= useStyles();
+  const url = 'https://code-challenges-backend.herokuapp.com/random/challenge';
+  
+  useEffect(() =>{
+    const fetchData = async () => { 
+      setLoading(true);
+      const respone = await axios.get(url);
+      setChallenge(`${respone.data}`);
+      console.log('this is the challeng:', respone.data);
+      setLoading(false);
+    };
+    fetchData();
+  },[fetching]);
+
+
+  return(
+    <>
+      <Grid 
+        direction="column"
+        justify="center" 
+        alignItems="center"
+      >
+        <Card className={styles.noteCard}>
+          {loading ? (
+            <CircularProgress variant="determinate" value={50} />
+          ):(
+            <Box className={styles.noteCardWriting}>{challenge}</Box>
+          )}
+          <Box className={styles.buttonBox}>
+            <Button className={styles.nButton} onClick={() => 
+              setFetching(!fetching)}> Next</Button>
+          </Box>
+        </Card>
+      </Grid>
+    </>
+  );
+}
+
+
+export default Challenge;
