38bdc8f351f715d835de1c83c8b4f19e188fc233
lizkavalski <lizkavalski@gmail.com>
2021-05-22 10:06:16 -0700

    working on style

---
+++ src/style/styleFooter.js
@@ -0,0 +1,18 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const styleFooter= makeStyles({
+  borderBox:{
+    textAlign: 'center',
+    // fontSize:'1.5em',
+    fontFamily:['Black Ops One','cursive'],
+    marginTop:20,
+    paddingBottom:20,
+  },
+  footnote:{
+    margin:0,
+    padding:0,
+  },
+});
+
+export default styleFooter;
